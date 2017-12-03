89a23103a03ac70587de19bac1f26e2be96dc832
Doktor Jakub Szajder <kuba.w108@gmail.com>
2017-12-03 20:43:03 +0100

    New scripts * add run.sh script to run application * add test.sh script to build tests and run them

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# @Author: Jakub Wysocki <jakub>
+# @Date:   2017-12-03T19:55:34+01:00
+# @Project: Strategy Game Engine
+# @Last modified by:   jakub
+# @Last modified time: 2017-12-03T19:57:14+01:00
+# @License: MIT License
+# @Copyright: Copyright (c) 2017
+
+./SGE/SGE
