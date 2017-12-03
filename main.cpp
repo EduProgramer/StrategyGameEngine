8682218164aadf0089e4283c226758d28c7ea995
Doktor Jakub Szajder <kuba.w108@gmail.com>
2017-12-03 20:42:00 +0100

    New project structure * gitignore update * new CMakeLists.txt * add googletest library

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
  * @Author: Jakub Wysocki
  * @Date:   2017-11-24T20:39:46+01:00
  * @Project: Strategy Game Engine
- * @Last modified by:   Jakub Wysocki
- * @Last modified time: 2017-11-24T21:57:20+01:00
+ * @Last modified by:   jakub
+ * @Last modified time: 2017-12-03T19:55:12+01:00
  * @License: MIT License
  * @Copyright: Copyright (c) 2017
  */
 
-#include "staticHeader.hpp"
 #include <iostream>
 
 int main() try
 {
     std::cout << "hello world" << std::endl;
-    sayHello();
     std::cout << std::endl;
     return 0;
 }
